675cea99b1f39d587ddd61327dc625dace501d9d
We2Am-BaSsem <58189568+We2Am-BaSsem@users.noreply.github.com>
2021-12-14 13:19:43 +0200

    Start Web Dicker

--- Web/cypress/integration/Tumbler/Basic.spec.js
+++ Web/cypress/integration/Tumbler/Basic.spec.js
@@ -1,18 +1,23 @@
-describe('Basic', () => {
-    beforeEach(() => {
-        /*
+describe("Basic", () => {
+  beforeEach(() => {
+    /*
             first you have to pass the url of the website, or the page you want
             to start your test with, you want to test
         */
-        cy.visit('https://www.tumblr.com/');
-    });
-    it('when reload the page the background changes', () => {
-        cy.get('img[loading="lazy"]').invoke('attr', 'srcset').then((prevBackground) => {
-            cy.reload();
-            cy.get('img[loading="lazy"]').invoke('attr', 'srcset').then(currBackground => {
-                expect(currBackground).to.not.eq(prevBackground);
-            });
-        });
-
-    })
+    cy.visit("/");
+  });
+  it("when reload the page the background changes", () => {
+    cy.get('section[data-testid="home-sec1"]')
+      .invoke("attr", "style")
+      .then((prevBackground) => {
+        cy.log(prevBackground);
+        cy.reload();
+        cy.get('section[data-testid="home-sec1"]')
+          .invoke("attr", "style")
+          .then((currBackground) => {
+            cy.log(prevBackground);
+            expect(currBackground).to.not.eq(prevBackground);
+          });
+      });
+  });
 });
