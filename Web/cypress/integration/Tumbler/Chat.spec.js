289ca007353aaab1f78cb3f39071dfe898f60938
ziad729 <55300514+ziad729@users.noreply.github.com>
2021-12-21 22:33:35 +0200

    creating chat script

---
+++ Web/cypress/integration/Tumbler/Chat.spec.js
@@ -0,0 +1,14 @@
+describe("Chatting", () => {
+
+    beforeEach(() => {
+      /*
+              first you have to pass the url of the website, or the page you want
+              to start your test with, you want to test
+          */
+      cy.visit("https://www.tumblr.com/");
+    });
+
+    it("sending messages", () => {
+      
+    });
+  });
