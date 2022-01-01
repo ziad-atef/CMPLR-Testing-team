0a760146137f6041ac07740b193d7e2ea7277f42
ziad729 <55300514+ziad729@users.noreply.github.com>
2022-01-01 14:35:03 +0200

    stress cancelled

--- Web/cypress/integration/Tumbler/Stress.spec.js
+++ Web/cypress/integration/Tumbler/Stress.spec.js
@@ -1,6 +1,15 @@
+let count = 0;
+
+const stress = () => {
+  count=count+1;
+  cy.log(count);
+  cy.visit('https://beta.cmplr.tech/');
+  stress();
+}
+
 describe("Stress", () => {
     it("Stress", () => {
-      cy.visit('https://beta.cmplr.tech/');
-    });
+      stress();
+    }); 
 });
   
